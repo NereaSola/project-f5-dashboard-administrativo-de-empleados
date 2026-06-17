be9d14d15987a5a8968e6f7d3bb89586ea287a0f
NereaSola <tu-email-de-NereaSola@gmail.com>
2026-06-17 21:17:50 +0200

    localstorage logout y responsive

--- src/js/events/btnlogoutevent.js
+++ src/js/events/btnlogoutevent.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const btnLogout = document.querySelector(".logout-btn");
 
     btnLogout.addEventListener("click", () => {
+        localStorage.removeItem("loggedUser");
         window.location.href = "../index.html";
     });
 });
